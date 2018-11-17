87cae6e97bee4b42224203a229d7111f80b08cbd
esadeli <efrat.sadeli@gmail.com>
2018-11-17 12:47:31 +0700

    Testing Validation OK

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict'
 
 require('dotenv').config()
+process.env["NTBA_FIX_319"] = 1;
 const express = require('express')
 const app = express()
+
+const Telegram = require('node-telegram-bot-api')
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new Telegram(token, { polling: true });
+const noSpace = require('./noSpace')
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
@@ -11,13 +17,27 @@ app.get('/', (req,res) => {
     console.log('OK Testing')
 })
 
-app.get('/test', (req,res)=> {
-    console.log('hi! this is testing response')
-    res.status(200).json({
-        msg: 'Hi This is Test'
-    })
-})
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    var message = msg.text.toString();
+    console.log(typeof message)
+    console.log('message-------', message,chatId)
+    if(message === '/start' || message.length === 0) {
+        bot.sendMessage(chatId, 'Hi! please send me the word you want to know');    
+    } else {
+        let check = noSpace(message)
+        if(check) {
+            bot.sendMessage(chatId, `
+                Please check your input!
+                \n- There should be no white spaces in your input
+                \n- I can only receive one word at a time
+            `);
+        } else {
+            bot.sendMessage(chatId, 'Message received');
+        }
+    }
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Listeing to PORt ', process.env.PORT)
+    console.log('Listening to PORT ', process.env.PORT)
 })
---
+++ noSpace.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function noSpace(input) {
+    return input.indexOf(' ') >=0   
+}
+
+// console.log(noSpace('bigmom'))
+// console.log(noSpace('greater bound'))
+
+module.exports = noSpace
